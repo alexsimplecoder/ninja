78af521f81b88e6f601a435d3cff879443b71e70
alexsimplecoder <alexchurilov814@gmail.com>
2026-03-17 19:40:30 +0800

    "guns for enemies added"

--- scripts/enemy.py
+++ scripts/enemy.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-from scripts import animation, player
+from scripts import animation, player, utils
 class Enemy(player.Player):
     def __init__(self, x, y, grid_tiles):
         super().__init__((x, y), grid_tiles)
@@ -12,8 +12,15 @@ class Enemy(player.Player):
         self.health = 100
         self.ml = True
         self.timer = random.randint(120, 264)
+        self.gun = utils.load_image("graph/images/gun.png", 3, color_key=(0, 0, 0))
+        self.lgun = pygame.transform.flip(self.gun, True, False)
+        self.lgun.set_colorkey((0, 0, 0))
     def render(self, screen, camera_x, camera_y):
         self.animations[self.state].render((self.x - camera_x, int(self.y - camera_y)), f"{self.dir}", screen)
+        if self.dir == "right":
+            screen.blit(self.gun, (self.x + 30 - camera_x, self.y + 30 - camera_y))
+        else:
+            screen.blit(self.lgun, (self.x - camera_x, self.y + 30 - camera_y))
     def ai_move(self):
         if self.coll_right:
             self.mr = False
